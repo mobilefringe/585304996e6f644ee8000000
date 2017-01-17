3b1407d23d077df23b72ab145851f2cda41dfcf6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-01-17 12:23:38 -0500

    Heritage Place Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -22,10 +22,8 @@ function init() {
     
     var feature_items = getFeatureList();
     var one_item = feature_items.slice(0,1);
-    console.log(one_item);
     renderFeatureItems('#feature_item','#feature_item_template', one_item);
     var two_items = feature_items.slice(1,3);
-    console.log(two_items);
     renderFeatureItems('#home_feature','#home_feature_template', two_items);
     
     var _fbq = window._fbq || (window._fbq = []);
