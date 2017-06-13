bd54e061fa77f7d5faed316d5e24b20bd52bcef4
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-06-13 17:19:10 -0400

    Heritage Place Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -527,37 +527,49 @@ function renderStoreDetails(container, template, collection, slug){
     $(container).html(item_rendered.join(''));
 }
 
-function renderStoreListCatetories(container, template, category_list,stores){
+function renderStoreList(container, template, collection, type){
+    var item_list = [];
     var item_rendered = [];
     var template_html = $(template).html();
-    Mustache.parse(template_html);   // optional, speeds up future use
-    var initial_id = 0;
-    var category_index = 0;
-    $.each(category_list , function( key, category ) {
-        var category_id = parseInt(category.id);
-        var category_name = category.name;
-        var current_id = category.id;
-        var count = 0;
-        
-        $.each( stores , function( i, store ) {
-            var store_category = store.categories;
-            var a = store.categories.indexOf(category_id);
-            
-            if (a > -1){
-                if (count == 0){
-                    store.show  = "display:block"; 
-                }else{
-                    store.show  = "display:none"; 
-                }
-                store.header = category_name;
-                store.block = category.id;
-                var rendered = Mustache.render(template_html,store);
-                item_rendered.push(rendered);
-                count += 1;
+    Mustache.parse(template_html);   // optional, speeds up future uses
+    var store_initial="";
+    $.each( collection , function( key, val ) {
+        if (type == "stores" || type == "category_stores"){
+            if(!val.store_front_url ||  val.store_front_url.indexOf('missing.png') > -1 || val.store_front_url.length === 0){
+                val.alt_store_front_url = "";
+            } else {
+                val.alt_store_front_url = getImageURL(val.store_front_url);    
             }
-        });
-        category_index += 1;
+        }
+
+        var current_initial = val.name[0];
+        val.cat_list = val.categories.join(',')
+        if(store_initial.toLowerCase() == current_initial.toLowerCase()){
+            val.initial = "";
+            val.show = "display:none;";
+        }
+        else{
+            val.initial = current_initial;
+            store_initial = current_initial;
+            val.show = "display:block;";
+        }
+        
+        if (val.total_published_promos > 0){
+            val.promotion_exist = "display:inline";
+        } else {
+            val.promotion_exist = "display:none";
+        }
+        
+        if(val.phone.length < 1){
+            val.phone_exist = "display:none";
+        }
+        
+        val.block = current_initial + '-block';
+        var rendered = Mustache.render(template_html,val);
+        var upper_current_initial = current_initial.toUpperCase();
+        item_rendered.push(rendered);
     });
+    $(container).show();
     $(container).html(item_rendered.join(''));
 }
 
