a00bc0ef2177f377245aba259b4a08ce0d7d8364
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-10-11 11:49:25 -0400

    Heritage Place Mall 1.1  /application.js

--- application.js
+++ application.js
@@ -423,9 +423,17 @@ function renderStoreList(container, template, collection, type){
                 val.alt_store_front_url = getImageURL(val.store_front_url);    
             }
         }
-
+        
+        if(val.categories != null){
+            // try {
+                val.cat_list = val.categories.join(',')
+            // }
+            // catch(err) {
+            //     console.log(err);
+            // }
+        }
+        
         var current_initial = val.name[0];
-        val.cat_list = val.categories.join(',')
         if(store_initial.toLowerCase() == current_initial.toLowerCase()){
             val.initial = "";
             val.show = "display:none;";
