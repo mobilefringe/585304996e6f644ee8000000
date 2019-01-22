aeaa12b1ceaff38346d9d46570858b670dbdbf29
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-01-22 12:47:06 -0500

    Heritage Place Mall 1.1  /helper.js

--- helper.js
+++ helper.js
@@ -30,53 +30,6 @@ function init() {
         }, 500)
     });
     
-    //Campaign Monitor Sign Up
-    // $('#popupForm').submit(function (e) {
-    //     if ($("#agree").prop("checked") != true){
-    //         alert("Please agree to the term and conditions.");
-    //         $("#agree").focus();
-    //         return false;
-    //     }
-    //     e.preventDefault();
-    //     $.getJSON(
-    //         this.action + "?callback=?",
-    //         $(this).serialize(),
-    //         function (data) {
-    //             if (data.Status === 400) {
-    //                 alert("Please try again later.");
-    //             } else { // 200
-    //                 $('#popupForm').trigger('reset');
-    //                 $("#success").fadeIn();
-                    
-    //                 setTimeout(function(){ 
-    //                     $(".modal-backdrop").remove();
-	   //                 $(".popup_home").remove();
-    //                 }, 2000);
-    //             }
-    //         });
-    // });
-    
-    //Campaign Monitor Sign Up
-    // $('#subForm').submit(function (e) {
-    //     if ($("#agree_terms").prop("checked") != true){
-    //         alert("Please agree to the term and conditions.");
-    //         $("#agree_terms").focus();
-    //         return false;
-    //     }
-    //     e.preventDefault();
-    //     $.getJSON(
-    //         this.action + "?callback=?",
-    //         $(this).serialize(),
-    //         function (data) {
-    //             if (data.Status === 400) {
-    //                 alert("Please try again later.");
-    //             } else { // 200
-    //                 $('#subForm').trigger('reset');
-    //                 $("#success_subscribe").fadeIn();
-    //             }
-    //         });
-    // });
-    
     //dynamically changing copyright year
     var current_year = moment().year();
     $("#current_year").text(current_year);
